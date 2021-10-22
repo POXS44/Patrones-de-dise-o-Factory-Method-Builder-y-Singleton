ac882b3d4ae78943eea10a0eedd3e67292552073
POXS44 <vzeledon7@gmail.com>
2021-10-21 20:26:47 -0600

    Clase cuadrado

---
+++ src/main/java/org/poxs44/MethodF/Cuadrado.java
@@ -0,0 +1,19 @@
+package org.poxs44.MethodF;
+
+import java.awt.*;
+
+public class Cuadrado extends Figuras{
+
+    public Cuadrado(int lado){
+        super(lado);
+    }
+
+    public double getArea(){
+        return (lado * lado);
+    }
+
+    public void dibujar(Graphics g, int x, int y){
+        g.setColor(Color.BLACK);
+        g.drawRect(x, y, lado, lado);
+    }
+}
