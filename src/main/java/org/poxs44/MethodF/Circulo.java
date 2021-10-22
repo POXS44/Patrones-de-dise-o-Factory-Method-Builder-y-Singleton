1e633c401bbced161f31c1a01e3ead65b0e97f3d
POXS44 <vzeledon7@gmail.com>
2021-10-21 20:16:31 -0600

    Circulo y Figuras

---
+++ src/main/java/org/poxs44/MethodF/Circulo.java
@@ -0,0 +1,18 @@
+package org.poxs44.MethodF;
+import java.awt.*;
+
+public class Circulo extends Figuras{
+
+    public Circulo(int lado){
+        super(lado);
+    }
+
+    public double getArea(){
+        return (Math.PI * lado * lado);
+    }
+
+    public void dibujar(Graphics g,int x, int y){
+        g.setColor(Color.BLACK);
+        g.drawArc(x, y, lado, lado, 0, 360);
+    }
+}
---
+++ src/main/java/org/poxs44/MethodF/Figuras.java
@@ -0,0 +1,13 @@
+package org.poxs44.MethodF;
+
+import java.awt.*;
+
+public abstract class Figuras {
+
+    int lado = 0;
+    public Figuras(int lado) {
+        this.lado = lado;
+    }
+    public abstract double getArea();
+    public abstract void dibujar(Graphics g, int x, int y);
+}
